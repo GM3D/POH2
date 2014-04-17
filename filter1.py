168398833f208d767d5f65a5e383c85b6b459149
Joji Monma (GM3D) <gmomma940@gmail.com>
2014-04-17 10:44:43 +0900

    filter1.py first working version

--- filter1.py
+++ filter1.py
@@ -4,19 +4,20 @@ import itertools
 int_ = int
 len_ = len
 imap = itertools.imap
+product = itertools.product
 split = str.split
+find = str.find
 append = list.append
 extend = list.extend
 sort = list.sort
 
 def input_data():
     content = split(sys.stdin.read().rstrip(), '\n')
-    print "content:\n%s" % content
     H, W = imap(int_, split(content[0]))
     grid = content[1:H + 1]
     N = int_(content[H + 1])
     widgets = imap(lambda l: map(int_, split(l)), content[H + 2:])
-    return H, W, N, grid, widgets
+    return (H, W, N, grid, widgets)
 
 def print_data(H, W, N, grid, widgets):
     print "H, W = %d, %d" % (H, W)
@@ -24,5 +25,23 @@ def print_data(H, W, N, grid, widgets):
     print "grid:\n%s" % grid
     print "widgets:\n%s" % list(widgets)
 
+def filter(h, w, i, j, H, W, grid):
+    lines = grid[i:i + h]
+    for line in lines:
+        if find(line, '1', j, j + w) >= 0:
+            return False
+    return True
+
+def filter_all(H, W, N, grid, widgets):
+    locs = []
+    for h, w in  widgets:
+        count = 0
+        for i, j in product(xrange(H - h + 1), xrange(W - w + 1)):
+            if filter(h, w, i, j, H, W, grid):
+                count += 1
+        append(locs, count)
+    return locs
+
 if __name__ == '__main__':
-    print_data(*input_data())
+    print '\n'.join(map(str, filter_all(*input_data())))
+    #print_data(*input_data())
